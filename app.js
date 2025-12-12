34a1131902b3d446f7cc163016adbc5e46dacbce
thanhhai-house <haivothanh0603@gmail.com>
2025-12-13 00:26:44 +0700

    Change API URL and enhance product rendering
    Updated API endpoint and modified product rendering logic.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const API = "https://script.google.com/macros/s/AKfycbws3KmLRQcpVpKcJuaKsDyLfiUHHm0fiNp3RtK0_WQFIsCdzZHTaQ2-uDD5nxQ7eI26Tg/exec";
+const API = "https://script.google.com/macros/s/AKfycbzfiyw-8CmWo4SzwGyereCQQog8R5f2sOVa-nCFN_sW-fZZ8ankH1LPqWkoKrrfMtF3Pw/exec";
 
 fetch(API+"?path=products")
   .then(r=>r.json())
-  .then(renderProducts);
+  .then(data=>{
+    const box=document.getElementById("products");
+    data.forEach(p=>{
+      box.innerHTML+=`
+        <div class="card">
+          <img src="${p.imageUrl||'https://via.placeholder.com/300'}">
+          <b>${p.name}</b>
+          <div>${p.brand} • ${p.category}</div>
+          <div>${p.price.toLocaleString()}đ</div>
+          <small>Tồn:${p.stock} | Bán:${p.sold}</small>
+        </div>`;
+    });
+  });
